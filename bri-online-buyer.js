c021ac74845bc93f56f76665d2c782148015f14e
James Hach <james.hach@gmail.com>
2018-04-18 13:29:25 -0700

    Added zip and cart total

--- bri-online-buyer.js
+++ bri-online-buyer.js
@@ -22,8 +22,16 @@ Bizrate Insights: [{
 
   _cnx.push(['mid', ##INSERT_STOREMID_HERE##]);  // your unique store MID
   _cnx.push(['surveyType', 'pos']);
-  _cnx.push(['orderId', '{{order.order_number}}']); // Shopify order number
-  _cnx.push(['emailAddress', '{{order.email}}']); // Shopify customer email address
+  _cnx.push(['pageId', 'orderConfirmation']);
+  _cnx.push(['orderId', '{{order.order_number}}']); // Shopify - order number
+  _cnx.push(['emailAddress', '{{order.email}}']); // Shopify - customer email address
+  _cnx.push(['customerId', '{{customer.id}}']); // Shopify - customer id
+  _cnx.push(['zip', '{{billing_address.zip}}']); // Shopify - customer billing zip
+  _cnx.push(['cartTotal', '{{order.total_price | money_without_currency}}']); // Shopify - Customer's cart total
+  _cnx.push(['couponApplied', '']);
+  _cnx.push(['webAnalyticsId', '']);
+  _cnx.push(['referrerPage', '']);
+  _cnx.push(['referrerUrl', '']);
 
   (function (w, d, t) {
     var s = d.createElement(t);
